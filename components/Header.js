35d5525de6e618124ec5684c1b74c43e77470fe9
s7manth <syalamarty002@gmail.com>
2023-03-22 01:31:40 +0800

    lose identity

--- components/Header.js
+++ components/Header.js
@@ -8,11 +8,11 @@ const NavBar = () => {
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.SEARCH, to: '/search', show: false },
-    { id: 3, name: locale.NAV.PROJECTS, to: '/projects', show: true },
-    { id: 4, name: locale.NAV.WORK, to: '/work', show: true },
-    { id: 5, name: locale.NAV.RSS, to: '/feed', show: true },
-    { id: 6, name: locale.NAV.CV, to: '/static/s7manth.pdf', show: true }
+    // { id: 2, name: locale.NAV.SEARCH, to: '/search', show: false },
+    // { id: 3, name: locale.NAV.PROJECTS, to: '/projects', show: true },
+    // { id: 4, name: locale.NAV.WORK, to: '/work', show: true },
+    // { id: 5, name: locale.NAV.RSS, to: '/feed', show: true },
+    // { id: 6, name: locale.NAV.CV, to: '/static/s7manth.pdf', show: true }
   ]
   return (
     <div className="flex-shrink-0">
